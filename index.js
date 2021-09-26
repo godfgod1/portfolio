74565494878017ba130a1ca99097af47e26fb938
godfgod1 <godfgod1@naver.com>
2021-09-26 12:02:27 +0900

    90%

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const buttons = document.querySelectorAll('button')
 
 function hiddenDiv(e){
     const  info = e.target.nextElementSibling
+    const  btn = e.target
     info.classList.toggle('hidden')
+    btn.classList.toggle('color')
 }
 
 
